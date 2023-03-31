1f8c217c868f86091a1a50e4d04e49167a27bb0d
suprabathk <suprabathkondapally@gmail.com>
2023-03-31 11:27:18 +0530

    Added TS Interface to TaskCard

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,6 +1,14 @@
-import "./TaskCard.css"
+import React from "react";
+import "./TaskCard.css";
 
-const TaskCard = (props) => {
+interface TaskCardInterface {
+    title: string,
+    completedAtDate?: string,
+    dueDate?: string,
+    assigneeName: string
+}
+
+const TaskCard = (props: TaskCardInterface) => {
     return (
         <div className='task-card border-black rounded-md border-2 my-2 py-2 px-4'>
             <p className="text-xl font-bold">{props.title}</p>
