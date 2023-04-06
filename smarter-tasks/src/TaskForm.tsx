bea6712b232100d94c2be5f4b6bcff60aff00cca
suprabathk <suprabathkondapally@gmail.com>
2023-04-06 23:20:54 +0530

    Migrated components to FC and added localStorage custom hook

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -11,89 +11,101 @@ interface TaskFormState {
   dueDate: string;
 }
 
-class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
-  constructor(props: TaskFormProps) {
-    super(props);
-    this.state = {
-      title: "",
-      description: "",
-      dueDate: "",
-    };
-  }
+const TaskForm = (props: TaskFormProps) => {
+  const [formState, setFormState] = React.useState<TaskFormState>({
+    title: "",
+    description: "",
+    dueDate: "",
+  });
 
-  addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+  const addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    if (this.state.title.length > 0 && this.state.dueDate.length > 0) {
-      this.props.addTask({
-        title: this.state.title,
-        description: this.state.description,
-        dueDate: this.state.dueDate,
-      });
-      this.setState({ title: "", description: "", dueDate: "" });
+    if (formState.title.length > 0 && formState.dueDate.length > 0) {
+      props.addTask(formState);
+      setFormState({ title: "", description: "", dueDate: "" });
     }
   };
 
-  titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    this.setState({ title: event.target.value });
+  const titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setFormState({ ...formState, title: event.target.value });
   };
-  descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    this.setState({ description: event.target.value });
+  const descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setFormState({ ...formState, description: event.target.value });
   };
-  dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    this.setState({ dueDate: event.target.value });
+  const dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setFormState({ ...formState, dueDate: event.target.value });
   };
 
-  render() {
-    return (
-      <div className="my-4">
-        <h3 className="text-slate-500 font-bold">Add item:</h3>
-        <form
-          className="flex flex-col gap-4 border my-2 border-slate-200 py-4 px-2 rounded-md shadow-md"
-          onSubmit={this.addTask}
-        >
-          <div className="flex flex-col">
-            <label htmlFor="todotitle">Title:</label>
-            <input
-              id="todoTitle"
-              className="border border-slate-200 rounded-md"
-              required
-              type="text"
-              value={this.state.title}
-              onChange={this.titleChanged}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="todoDescription">Description:</label>
-            <input
-              id="todoDescription"
-              className="border border-slate-200 rounded-md"
-              type="text"
-              value={this.state.description}
-              onChange={this.descriptionChanged}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="todoDueDate">Due date:</label>
-            <input
-              id="todoDueDate"
-              className="border border-slate-200 rounded-md"
-              required
-              type="date"
-              value={this.state.dueDate}
-              onChange={this.dueDateChanged}
-            />
-          </div>
+  return (
+    <form onSubmit={addTask}>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoTitle"
+            name="todoTitle"
+            type="text"
+            value={formState.title}
+            onChange={titleChanged}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+          />
+          <label
+            htmlFor="todoTitle"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Todo Title
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoDescription"
+            name="todoDescription"
+            type="text"
+            value={formState.description}
+            onChange={descriptionChanged}
+            placeholder=" "
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+          <label
+            htmlFor="todoDescription"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Description
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoDueDate"
+            name="todoDueDate"
+            type="date"
+            value={formState.dueDate}
+            onChange={dueDateChanged}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="todoDueDate"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Due Date
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
           <button
-            id="addTaskButton"
-            className="border border-green-400 rounded-md"
             type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add item
           </button>
-        </form>
+        </div>
       </div>
-    );
-  }
-}
+    </form>
+  );
+};
 
 export default TaskForm;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -2,27 +2,24 @@ import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
 
-interface Props {
+interface TaskListProps {
   tasks: TaskItem[];
 }
 
-interface State {}
-
-class TaskList extends React.Component<Props, State> {
-  render() {
-    return (
-      <div>
-        <h3 className="text-slate-500 font-bold">Items:</h3>
-        {this.props.tasks.map((task) => (
-          <Task
-            title={task.title}
-            description={task.description}
-            dueDate={task.dueDate}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const TaskList = (props: TaskListProps) => {
+  return (
+    <div>
+      <h3 className="text-slate-500 font-bold">Items:</h3>
+      {props.tasks.map((task, idx) => (
+        <Task
+          key={idx}
+          title={task.title}
+          description={task.description}
+          dueDate={task.dueDate}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default TaskList;
