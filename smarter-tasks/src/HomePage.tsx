0696fafe8e9e4e2ddf2db74ce2ea4fe8e777eb2c
suprabathk <suprabathkondapally@gmail.com>
2023-05-28 11:28:55 +0530

    Added React router

---
+++ smarter-tasks/src/HomePage.tsx
@@ -0,0 +1,13 @@
+// HomePage.tsx
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <p>Welcome to the Task Manager application!</p>
+    </div>
+  );
+};
+
+export default HomePage;
