a587ec82b7e7bb04e38ef74dab306a8290ea4ed2
suprabathk <suprabathkondapally@gmail.com>
2023-05-28 12:19:58 +0530

    Added auth and 404

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 
 import Header from "./Header";
 import HomePage from "./HomePage";
 import TaskApp from "./TaskApp";
 import TaskDetailsPage from "./TaskDetailsPage";
+import Signin from "./Signin";
+import { ProtectedRoute } from "./ProtectedRoute";
+import NotFound from "./NotFound";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div>
-      <Header />
+      {location.pathname !== "/signin" && location.pathname !== "/notfound" && (
+        <Header />
+      )}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tasks" element={<TaskApp />} />
-        <Route path="/tasks/:id" element={<TaskDetailsPage />} />
+        <Route path="/" element={<ProtectedRoute element={<HomePage />} />} />
+        <Route
+          path="/tasks"
+          element={<ProtectedRoute element={<TaskApp />} />}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<ProtectedRoute element={<TaskDetailsPage />} />}
+        />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="*" element={<Navigate to={"/notfound"} />} />
       </Routes>
     </div>
   );
--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TaskCard.css";
+import { Link } from "react-router-dom";
 
 interface TaskProps {
   title: string;
@@ -13,7 +14,9 @@ const Task = (props: TaskProps) => {
   return (
     <li className="TaskItem shadow-md border border-slate-200 px-4 py-2 my-2 rounded-md list-none flex justify-between items-center">
       <div>
-        <h2 className="text-base font-bold my-1">{props.title} </h2>
+        <Link to={`/tasks/${props.id}`}>
+          <h2 className="text-base font-bold my-1">{props.title} </h2>
+        </Link>
         <p className="text-sm text-slate-500">{props.dueDate}</p>
         <p className="text-sm text-slate-500">
           Description: {props.description}
