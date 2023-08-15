5d871d18b4ea88b7e0c5e03a64c7280ba8a12626
suprabathk <suprabathkondapally@gmail.com>
2023-08-15 13:09:19 +0530

    Fixed Modal Route issues

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -1,16 +1,16 @@
-import { Navigate, Outlet, createBrowserRouter } from "react-router-dom";
-import ProjectContainer from "../pages/projects/ProjectContainer";
-import AccountLayout from "../layouts/account"
-import ProtectedRoutes from "./ProtectedRoutes"
-import Signin from "../pages/signin"
-import Signup from "../pages/signup"
-import Projects from "../pages/projects"
-import Members from "../pages/members"
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import AccountLayout from "../layouts/account";
+import ProtectedRoutes from "./ProtectedRoutes";
+import Signin from "../pages/signin";
+import Signup from "../pages/signup";
+import Projects from "../pages/projects";
+import Members from "../pages/members";
 import Logout from "../pages/logout";
-import Notfound from "../pages/Notfound";
-import ProjectDetails from "../pages/project_details/ProjectDetails";
+import ProjectContainer from "../pages/projects/ProjectContainer";
+import ProjectDetails from "../pages/project_details";
 import NewTask from "../pages/tasks/NewTask";
 import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
+import Notfound from "../pages/Notfound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
