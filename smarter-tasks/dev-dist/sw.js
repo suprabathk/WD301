06e198c665ca7c5d7e4d423712bbe3eb84ef7852
suprabathk <suprabathkondapally@gmail.com>
2023-08-18 21:13:35 +0530

    Lighthouse enhancements

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-715e063a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.neupj01or8"
+    "revision": "0.2ernt5q04r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
