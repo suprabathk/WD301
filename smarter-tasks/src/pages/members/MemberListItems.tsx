adb778794720a659bfff19ce9ef2abf2385dc053
suprabathk <suprabathkondapally@gmail.com>
2023-08-18 21:25:20 +0530

    Added classnames for bot review

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -1,40 +1,60 @@
 import React from "react";
 
-import { useMembersDispatch, useMembersState } from "../../context/members/context";
+import {
+  useMembersDispatch,
+  useMembersState,
+} from "../../context/members/context";
 import { deleteMember } from "../../context/members/actions";
 
 export default function MemberListItems() {
-    const dispatchMembers = useMembersDispatch();
-    let state: any = useMembersState();
-    // console.log({ state });
+  const dispatchMembers = useMembersDispatch();
+  let state: any = useMembersState();
+  // console.log({ state });
 
-    const { members, isLoading, isError, errorMessage } = state
-    console.log(members);
+  const { members, isLoading, isError, errorMessage } = state;
+  console.log(members);
 
-    if (members.length === 0 && isLoading) {
-        return <span>Loading...</span>;
-    }
+  if (members.length === 0 && isLoading) {
+    return <span>Loading...</span>;
+  }
 
-    if (isError) {
-        return <span>{errorMessage}</span>;
-    }
+  if (isError) {
+    return <span>{errorMessage}</span>;
+  }
 
-    return (
-        <>
-            {members.map((member: any) => (
-                <div key={member.id} className="member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <div className="flex justify-between items-center">
-                        <h5 className="mb-1 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
-                        <button onClick={() => deleteMember(dispatchMembers, member.id)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hover:text-red-500 transition-all">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg>
-                        </button>
-
-                    </div>
-                    <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-400 dark:text-white">{member.email}</h5>
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {members.map((member: any) => (
+        <div
+          key={member.id}
+          className="supense-loading member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <div className="flex justify-between items-center">
+            <h5 className="mb-1 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
+              {member.name}
+            </h5>
+            <button onClick={() => deleteMember(dispatchMembers, member.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 hover:text-red-500 transition-all"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+          </div>
+          <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-400 dark:text-white">
+            {member.email}
+          </h5>
+        </div>
+      ))}
+    </>
+  );
 }
--- smarter-tasks/src/pages/project_details/ProjectDetails.tsx
+++ smarter-tasks/src/pages/project_details/ProjectDetails.tsx
@@ -8,44 +8,44 @@ import { refreshTasks } from "../../context/task/actions";
 import { useProjectsState } from "../../context/projects/context";
 
 const ProjectDetails = () => {
-    const tasksState = useTasksState();
-    const taskDispatch = useTasksDispatch();
-    const projectState = useProjectsState();
-    let { projectID } = useParams();
-    useEffect(() => {
-        if (projectID) refreshTasks(taskDispatch, projectID);
-    }, [projectID, taskDispatch]);
-    const selectedProject = projectState?.projects.filter(
-        (project) => `${project.id}` === projectID
-    )?.[0];
+  const tasksState = useTasksState();
+  const taskDispatch = useTasksDispatch();
+  const projectState = useProjectsState();
+  let { projectID } = useParams();
+  useEffect(() => {
+    if (projectID) refreshTasks(taskDispatch, projectID);
+  }, [projectID, taskDispatch]);
+  const selectedProject = projectState?.projects.filter(
+    (project) => `${project.id}` === projectID
+  )?.[0];
 
-    if (!selectedProject) {
-        return <>No such Project!</>;
-    }
+  if (!selectedProject) {
+    return <>No such Project!</>;
+  }
 
-    if (tasksState.isLoading) {
-        return <>Loading...</>;
-    }
-    return (
-        <>
-            <div className="flex justify-between">
-                <h2 className="text-2xl font-medium tracking-tight text-slate-700">
-                    {selectedProject.name}
-                </h2>
-                <Link to={`tasks/new`}>
-                    <button
-                        id="newTaskBtn"
-                        className="rounded-md bg-blue-600 px-4 py-2 m-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                    >
-                        New Task
-                    </button>
-                </Link>
-            </div>
-            <div className="grid grid-cols-1 gap-2">
-                <DragDropList data={tasksState.projectData} />
-            </div>
-        </>
-    );
+  if (tasksState.isLoading) {
+    return <>Loading...</>;
+  }
+  return (
+    <>
+      <div className="supense-loading flex justify-between">
+        <h2 className="text-2xl font-medium tracking-tight text-slate-700">
+          {selectedProject.name}
+        </h2>
+        <Link to={`tasks/new`}>
+          <button
+            id="newTaskBtn"
+            className="rounded-md bg-blue-600 px-4 py-2 m-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            New Task
+          </button>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 gap-2">
+        <DragDropList data={tasksState.projectData} />
+      </div>
+    </>
+  );
 };
 
 export default ProjectDetails;
