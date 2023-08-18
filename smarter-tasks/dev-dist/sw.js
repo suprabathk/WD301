3b3e1a8ab1dfce8bd2ea4cb39b03c058d26cf1ef
suprabathk <suprabathkondapally@gmail.com>
2023-08-18 20:54:08 +0530

    Added PWA

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-715e063a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kaigl9uotog"
+    "revision": "0.neupj01or8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
